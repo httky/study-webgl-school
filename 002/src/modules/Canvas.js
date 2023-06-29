c3637a39f3c3777be41e0a1097f4f90d9ec72ad2
httky <takuyahata.tech@gmaiil.com>
2023-06-29 14:52:36 +0900

    [update] 002 扇風機作る

--- 002/src/modules/Canvas.js
+++ 002/src/modules/Canvas.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
-import { map } from '@/utils/'
+import { map, degToRad } from '@/utils/'
 
 export class Canvas {
   constructor({ container }) {
@@ -35,7 +35,9 @@ export class Canvas {
       dLightVisible: false,
       aLightColor: 0xffffff,
       aLightIntensity: 0.2,
-      axesHelperVisible: false,
+      helperVisible: true,
+      isSwinging: false,
+      fanPower: 0.00,
     }
     this.stats = undefined
     this.gui = undefined
@@ -49,8 +51,12 @@ export class Canvas {
     this.material = undefined
     this.boxGeometry = undefined
     this.meshes = undefined
+    this.fanHead = undefined
+    this.fan = undefined
     this.controls = undefined
     this.axesHelper = undefined
+    this.countTime = 0
+    this.arrowHelper = undefined
 
     // bind
     this.update = this.update.bind(this)
@@ -115,11 +121,12 @@ export class Canvas {
 
     // axesHelper
     this.axesHelper = new THREE.AxesHelper(5)
-    this.axesHelper.visible = this.params.axesHelperVisible
+    this.axesHelper.visible = this.params.helperVisible
     this.scene.add(this.axesHelper)
 
     this.updateSize()
     this.meshes = this.createMeshes()
+    console.log(this.meshes.children)
     this.scene.add(this.meshes)
     this.attachEvents()
   }
@@ -150,10 +157,13 @@ export class Canvas {
 
     // gui
     this.gui = new GUI()
+    this.gui.add(this.params, 'isSwinging').name('isSwinging')
+    this.gui.add(this.params, 'fanPower', 0.0, 0.1).name('fanPower')
 
-    const axesHelperFolder = this.gui.addFolder('axesHelper')
-    axesHelperFolder.add(this.params, 'axesHelperVisible').name('visible').onChange(() => {
-      this.axesHelper.visible = this.params.axesHelperVisible
+    const helperFolder = this.gui.addFolder('helper')
+    helperFolder.add(this.params, 'helperVisible').name('visible').onChange(() => {
+      this.axesHelper.visible = this.params.helperVisible
+      this.arrowHelper.visible = this.params.helperVisible
     })
   }
   /**
@@ -161,9 +171,26 @@ export class Canvas {
    */
   createMeshes() {
     const group = new THREE.Group()
-    // material
+
+    // material 共通
     this.material = new THREE.MeshPhongMaterial({ color: this.params.materialColor })
-    // geometry
+
+    // 台座
+    const pedestalGeometry = new THREE.CylinderGeometry(0.5, 0.5, 0.05, 16)
+    const pedestalMesh = new THREE.Mesh(pedestalGeometry, this.material)
+    pedestalMesh.position.set(0, -1.0, 0)
+    group.add(pedestalMesh)
+
+    // 支柱
+    const strutGeometry = new THREE.CylinderGeometry(0.15, 0.15, 1, 16)
+    const strutMesh = new THREE.Mesh(strutGeometry, this.material)
+    strutMesh.position.set(0, -0.5, 0)
+    group.add(strutMesh)
+
+    // ファン頭部
+    this.fanHead = new THREE.Group()
+
+    // モーター
     this.boxGeometry = new THREE.BoxGeometry(
       this.params.boxSize,
       this.params.boxSize,
@@ -171,8 +198,39 @@ export class Canvas {
     )
 
     const mesh = new THREE.Mesh(this.boxGeometry, this.material)
+    this.fanHead.add(mesh)
 
-    group.add(mesh)
+    // ファン
+    this.fan = new THREE.Group()
+    this.fan.position.set(0, 0, 0.28)
+    this.fan.rotation.x = degToRad(90)
+    const fanGeometry = new THREE.CylinderGeometry(0.2, 0.2, 0.3, 16)
+    const fanMesh = new THREE.Mesh(fanGeometry, this.material)
+    fanMesh.scale.set(0.6, 0.6, 0.6)
+
+    const panelNum = 8
+    const panelGeometry = new THREE.BoxGeometry(0.16, 0.03, 1.1)
+    for (let i = 0; i < panelNum; i++) {
+      const panel = new THREE.Mesh(panelGeometry, this.material)
+      panel.rotation.y = degToRad(360 / panelNum * i)
+      // panel.position.set(0, 0, 0.0)
+      this.fan.add(panel)
+    }
+
+    this.fan.add(fanMesh)
+    this.fanHead.add(this.fan)
+
+    // ベクトルヘルパー
+    const dir = new THREE.Vector3(...this.fan.position)
+    dir.normalize()
+    const origin = new THREE.Vector3(0, 0, 0)
+    const length = 1
+    const hex = 0xffff00
+    this.arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex)
+    this.arrowHelper.visible = this.params.helperVisible
+    this.fanHead.add(this.arrowHelper)
+
+    group.add(this.fanHead)
 
     return group
   }
@@ -214,9 +272,12 @@ export class Canvas {
   update() {
     this.controls.update()
 
-    this.meshes.children.forEach((mesh) => {
-      mesh.rotation.y += 0.01
-    })
+    this.fan.rotation.y += this.params.fanPower
+
+    if (this.params.isSwinging) {
+      this.countTime += 0.01
+      this.fanHead.rotation.y = Math.sin(this.countTime * 0.7)
+    }
 
     this.directionalLightHelper.update()
 
---
+++ 002/src/utils/degToRad.js
@@ -0,0 +1,8 @@
+/**
+ * degToRad
+ * @param {number} degrees
+ * @return {number}
+ */
+export function degToRad(degrees) {
+  return degrees * Math.PI / 180
+}
